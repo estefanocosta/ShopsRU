318134713e457b7e1255ff0fa82730d8a521c37e
evren <evren@EVRENAKTAS>
2023-07-31 19:40:43 +0300

    last commit

---
+++ ShopsRU.Application/Contract/Request/Log/CreateLogRequest.cs
@@ -0,0 +1,36 @@
+﻿using ShopsRU.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopsRU.Application.Contract.Request.Log
+{
+    public class CreateLogRequest
+    {
+        public int UserID { get; set; }
+        public string Message { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Method { get; set; }
+        public string Trace { get; set; }
+        public DateTime PostDate { get; set; }
+        public int ErrorCode { get; set; }
+        public ShopsRU.Domain.Entities.Log MapToEntity()
+        {
+            return new Domain.Entities.Log()
+            {
+                Message = this.Message,
+                Controller = this.Controller,
+                Method = this.Method,
+                PostDate = DateTime.Now,
+                Action = this.Action,
+                Trace = this.Trace,
+                ErrorCode = this.ErrorCode,
+                UserID = 5
+            };
+        }
+    }
+}
